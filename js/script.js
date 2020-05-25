83cdd9df34a58940c3d249374ed1c2636f0dcb2b
abimaelst <abimaelst@live.com>
2020-05-25 01:04:22 -0300

    Finish User Results

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 let tabSearchUsersFound = null;
+let searchInput = null;
+let userSearchInput = '';
 
-let allUsers = null;
+let allUsers = [];
+let usersFound = [];
 
 window.addEventListener('load', () => {
   tabSearchUsersFound = document.querySelector('#tabSearchUsersFound');
+  searchInput = document.querySelector('#searchInput');
   fetchUsers();
+  resultSearchUsers();
 });
 
 const fetchUsers = async () => {
@@ -12,16 +17,45 @@ const fetchUsers = async () => {
     'https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo'
   );
   const json = await res.json();
+
   allUsers = json.results.map((user) => {
-    const { name, picture, dob, login } = user;
+    const { name, picture, dob, login, gender } = user;
     return {
       login: login.username,
       name: name.first + ' ' + name.last,
       dob: dob.age,
       picture: picture.thumbnail,
+      gender,
     };
   });
-  render();
+};
+
+function preventFormSubmit() {
+  function handleFormSubmit(event) {
+    event.preventDefault();
+  }
+  let form = document.querySelector('#searchInput');
+  form.addEventListener('submit', handleFormSubmit);
+}
+
+const resultSearchUsers = () => {
+  function handleSearch(event) {
+    if (event.key === 'Enter') {
+      userSearchInput = event.target.value;
+
+      for (i = 0; i < 10; i++) {
+        let name = allUsers[i].name;
+
+        if (name.toUpperCase().indexOf(userSearchInput.toUpperCase()) > -1) {
+          usersFound = [...usersFound, allUsers[i]];
+        }
+      }
+      render();
+    }
+    //Limpando Variável da Busca após o resultado
+    usersFound = [];
+  }
+  searchInput.addEventListener('keyup', handleSearch);
 };
 
 const render = () => {
@@ -29,12 +63,16 @@ const render = () => {
 };
 
 const renderSearchResult = () => {
-  let usersHTML = '<div>';
+  let usersHTML = '';
+  if (usersFound.length === 0) {
+    console.log('aqui');
+    usersHTML = '<h2>Nenhum Usuário Encontrado</h2>';
+  } else {
+    usersHTML = '<div>';
 
-  allUsers.forEach((user) => {
-    const { name, dob, picture, login } = user;
-    const space = ' ';
-    const userHTML = `
+    usersFound.forEach((user) => {
+      const { name, dob, picture, login } = user;
+      const userHTML = `
       <div class='users'>
         <div>
           <img src='${picture}' name='${name}' />
@@ -47,10 +85,10 @@ const renderSearchResult = () => {
         </div>
       </div>
     `;
-    usersHTML += userHTML;
-  });
+      usersHTML += userHTML;
+    });
 
-  usersHTML += '</div>';
-
+    usersHTML += '</div>';
+  }
   tabSearchUsersFound.innerHTML = usersHTML;
 };
