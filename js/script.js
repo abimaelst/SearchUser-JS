1ac56df0a2b7e88bbfe06c31b402927ba2165656
abimaelst <abimaelst@live.com>
2020-05-26 14:25:34 -0300

    finish project

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 let tabSearchUsersFound = null;
 let searchInput = null;
 let userSearchInput = '';
+let submitButton = null;
+let loading = null;
 
 let qtdFound = null;
 let tittleStatistics = null;
@@ -19,6 +21,8 @@ window.onload = () => {
   tabSearchUsersFound = document.querySelector('#tabSearchUsersFound');
   searchInput = document.querySelector('#searchInput');
   tittleStatistics = document.querySelector('#tittleStatistics');
+  submitButton = document.querySelector('#submitButton');
+  loading = document.querySelector('#loading');
 
   qtdFound = document.querySelector('#qtdFound');
   countResultUsers = document.querySelector('#countResultUsers');
@@ -30,9 +34,25 @@ window.onload = () => {
   genderMen = document.querySelector('#genderMen');
   genderWomen = document.querySelector('#genderWomen');
 
+  numberFormat = Intl.NumberFormat('pt-BR');
+
+  submitButton.disabled = true;
+
+  let ponto = '❱';
+  const interval = setInterval(() => {
+    loading.innerHTML = `<h5>Carregando${ponto}</h5>`;
+    ponto += '❱';
+
+    if (ponto === '❱❱❱❱❱❱') {
+      loading.textContent = '';
+      this.clearInterval(interval);
+    }
+  }, 1000);
+
   fetchUsers();
   resultSearchUsers();
   resetSearchArray();
+  buttonSubmit();
 };
 
 const fetchUsers = async () => {
@@ -56,6 +76,7 @@ const fetchUsers = async () => {
 function preventFormSubmit() {
   function handleFormSubmit(event) {
     event.preventDefault();
+    console.log(event);
   }
   let form = document.querySelector('#searchInput');
   form.addEventListener('submit', handleFormSubmit);
@@ -63,10 +84,11 @@ function preventFormSubmit() {
 
 const resultSearchUsers = () => {
   function handleSearch(event) {
-    if (event.key === 'Enter') {
+    submitButton.disabled = false;
+    if (event.key === 'Enter' && searchInput.value !== '') {
       userSearchInput = event.target.value;
 
-      for (i = 0; i < 10; i++) {
+      for (i = 0; i < allUsers.length; i++) {
         let name = allUsers[i].name;
 
         if (name.toUpperCase().indexOf(userSearchInput.toUpperCase()) > -1) {
@@ -74,6 +96,8 @@ const resultSearchUsers = () => {
         }
       }
       render();
+    } else if (searchInput.value === '') {
+      submitButton.disabled = true;
     }
     //Limpando Variável da Busca após o resultado
     resetSearchArray();
@@ -81,6 +105,23 @@ const resultSearchUsers = () => {
   searchInput.addEventListener('keyup', handleSearch);
 };
 
+const buttonSubmit = () => {
+  function handleButton(event) {
+    userSearchInput = searchInput.value;
+
+    for (i = 0; i < allUsers.length; i++) {
+      let name = allUsers[i].name;
+
+      if (name.toUpperCase().indexOf(userSearchInput.toUpperCase()) > -1) {
+        usersFound = [...usersFound, allUsers[i]];
+      }
+    }
+    render();
+  }
+  //Limpando Variável da Busca após o resultado
+  resetSearchArray();
+  submitButton.addEventListener('click', handleButton);
+};
 const render = () => {
   SearchResult();
   resultStatistics();
@@ -134,7 +175,9 @@ const resultStatistics = () => {
     genderMen.innerHTML = `Sexo masculino: <span id="genderManResult">${men.length}</span>`;
     genderWomen.innerHTML = `Sexo Feminino: <span id="genderManResult">${women.length}</span>`;
     SumAges.innerHTML = `Soma das idades: <span>${sumAge}</span>`;
-    averageAges.innerHTML = `Soma das idades: <span>${sumAge / totalResult}
+    averageAges.innerHTML = `Soma das idades: <span>${formatNumber(
+      sumAge / totalResult
+    )}
     </span>`;
     qtdFound.innerHTML = `${totalResult} Usuário(s) Encontrado(s)`;
   } else {
@@ -149,4 +192,8 @@ const resultStatistics = () => {
 
 const resetSearchArray = () => {
   usersFound = [];
+};
+
+const formatNumber = (number) => {
+  return numberFormat.format(number);
 };
